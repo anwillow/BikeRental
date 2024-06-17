b2c563786a35cb246d75d5fb2b4cbb8f0c552009
Konstantin <129529092+anwillow@users.noreply.github.com>
2024-06-17 16:04:28 +0300

    Add files via upload

---
+++ BikeRental/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BikeRentalApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            dataGridView2 = new DataGridView();
+            button7 = new Button();
+            dataGridView3 = new DataGridView();
+            dataGridView4 = new DataGridView();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(139, 185);
+            button1.Name = "button1";
+            button1.Size = new Size(208, 45);
+            button1.TabIndex = 0;
+            button1.Text = "Guest";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(469, 185);
+            button2.Name = "button2";
+            button2.Size = new Size(208, 45);
+            button2.TabIndex = 1;
+            button2.Text = "Admin";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(236, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(344, 425);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.Visible = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(639, 371);
+            button3.Name = "button3";
+            button3.Size = new Size(149, 42);
+            button3.TabIndex = 3;
+            button3.Text = "Back";
+            button3.UseVisualStyleBackColor = true;
+            button3.Visible = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(78, 185);
+            button4.Name = "button4";
+            button4.Size = new Size(152, 45);
+            button4.TabIndex = 4;
+            button4.Text = "Bike DB";
+            button4.UseVisualStyleBackColor = true;
+            button4.Visible = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(297, 185);
+            button5.Name = "button5";
+            button5.Size = new Size(154, 45);
+            button5.TabIndex = 5;
+            button5.Text = "Trip DB";
+            button5.UseVisualStyleBackColor = true;
+            button5.Visible = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(523, 185);
+            button6.Name = "button6";
+            button6.Size = new Size(154, 45);
+            button6.TabIndex = 6;
+            button6.Text = "Account DB";
+            button6.UseVisualStyleBackColor = true;
+            button6.Visible = false;
+            button6.Click += button6_Click;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(12, 0);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(776, 230);
+            dataGridView2.TabIndex = 7;
+            dataGridView2.Visible = false;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(639, 371);
+            button7.Name = "button7";
+            button7.Size = new Size(149, 42);
+            button7.TabIndex = 8;
+            button7.Text = "Back";
+            button7.UseVisualStyleBackColor = true;
+            button7.Visible = false;
+            button7.Click += button7_Click;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(12, 0);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.RowTemplate.Height = 25;
+            dataGridView3.Size = new Size(776, 230);
+            dataGridView3.TabIndex = 9;
+            dataGridView3.Visible = false;
+            // 
+            // dataGridView4
+            // 
+            dataGridView4.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView4.Location = new Point(12, 0);
+            dataGridView4.Name = "dataGridView4";
+            dataGridView4.RowTemplate.Height = 25;
+            dataGridView4.Size = new Size(776, 230);
+            dataGridView4.TabIndex = 10;
+            dataGridView4.Visible = false;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(639, 323);
+            button8.Name = "button8";
+            button8.Size = new Size(149, 42);
+            button8.TabIndex = 11;
+            button8.Text = "Save changes";
+            button8.UseVisualStyleBackColor = true;
+            button8.Visible = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(639, 323);
+            button9.Name = "button9";
+            button9.Size = new Size(149, 42);
+            button9.TabIndex = 12;
+            button9.Text = "Save Changes";
+            button9.UseVisualStyleBackColor = true;
+            button9.Visible = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(639, 323);
+            button10.Name = "button10";
+            button10.Size = new Size(149, 42);
+            button10.TabIndex = 13;
+            button10.Text = "Save Changes";
+            button10.UseVisualStyleBackColor = true;
+            button10.Visible = false;
+            button10.Click += button10_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(dataGridView4);
+            Controls.Add(dataGridView3);
+            Controls.Add(button7);
+            Controls.Add(dataGridView2);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private DataGridView dataGridView2;
+        private Button button7;
+        private DataGridView dataGridView3;
+        private DataGridView dataGridView4;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+    }
+}
---
+++ BikeRental/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace BikeRentalApp
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\dozor\source\repos\BikeRentalApp\BikeRentalApp\Database3.accdb;";
+        private OleDbConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new OleDbConnection(connectionString);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string password = Microsoft.VisualBasic.Interaction.InputBox("Please, enter admin password.", "Admin confirmation", "");
+            if (password == "1231")
+            {
+                MessageBox.Show("Welcome back, Admin!");
+
+                dataGridView4.Visible = false;
+                dataGridView3.Visible = false;
+                dataGridView1.Visible = false;
+                dataGridView2.Visible = false;
+                button3.Visible = true;
+                button1.Visible = false;
+                button2.Visible = false;
+                button4.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+                button7.Visible = false;
+                button8.Visible = false;
+                button9.Visible = false;
+                button10.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Incorrect password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                dataGridView4.Visible = false;
+                dataGridView3.Visible = false;
+                dataGridView1.Visible = false;
+                dataGridView2.Visible = false;
+                button3.Visible = false;
+                button1.Visible = true;
+                button2.Visible = true;
+                button4.Visible = false;
+                button5.Visible = false;
+                button6.Visible = false;
+                button7.Visible = false;
+                button8.Visible = false;
+                button9.Visible = false;
+                button10.Visible = false;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenConnection();
+
+            string query = "SELECT Type, City, Status FROM Bike WHERE Status = 'available'";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            dataGridView2.ReadOnly = true;
+            dataGridView3.ReadOnly = true;
+            dataGridView4.ReadOnly = true;
+
+            dataGridView4.Visible = false;
+            dataGridView3.Visible = false;
+            dataGridView1.Visible = true;
+            dataGridView2.Visible = false;
+            button3.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            button10.Visible = false;
+
+            CloseConnection();
+        }
+
+
+        public void OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connecting with DB opened.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while opening connection with DB " + ex.Message);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                Console.WriteLine("Connecting with DB closed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while closing connection with DB: " + ex.Message);
+            }
+        }
+
+        public void ExecuteQuery(string query)
+        {
+            try
+            {
+                OleDbCommand command = new OleDbCommand(query, connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("Запрос выполнен успешно.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при выполнении запроса: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView4.Visible = false;
+            dataGridView3.Visible = false;
+            dataGridView1.Visible = false;
+            dataGridView2.Visible = false;
+            button3.Visible = false;
+            button1.Visible = true;
+            button2.Visible = true;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            button10.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenConnection();
+
+            string query = "SELECT * FROM Bike";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView2.DataSource = dataTable;
+            dataGridView2.ReadOnly = false;
+            dataGridView2.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+            foreach (DataGridViewColumn column in dataGridView2.Columns)
+            {
+                column.Visible = false;
+            }
+
+            dataGridView2.Columns["Type"].Visible = true;
+            dataGridView2.Columns["City"].Visible = true;
+            dataGridView2.Columns["Status"].Visible = true;
+
+            dataGridView4.Visible = false;
+            dataGridView3.Visible = false;
+            dataGridView2.Visible = true;
+            dataGridView1.Visible = false;
+            button3.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = true;
+            button8.Visible = true;
+            button9.Visible = false;
+            button10.Visible = false;
+
+            CloseConnection();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenConnection();
+
+            string query = "SELECT * FROM Trip";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView3.DataSource = dataTable;
+            dataGridView3.ReadOnly = false;
+            dataGridView4.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+            foreach (DataGridViewColumn column in dataGridView3.Columns)
+            {
+                column.Visible = false;
+            }
+
+            dataGridView3.Columns["Bike_ID"].Visible = true;
+            dataGridView3.Columns["Acc_ID"].Visible = true;
+            dataGridView3.Columns["City"].Visible = true;
+
+            dataGridView4.Visible = false;
+            dataGridView3.Visible = true;
+            dataGridView2.Visible = false;
+            dataGridView1.Visible = false;
+            button3.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = true;
+            button8.Visible = false;
+            button9.Visible = true;
+            button10.Visible = false;
+
+            CloseConnection();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenConnection();
+
+            string query = "SELECT * FROM Acc";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView4.DataSource = dataTable;
+            dataGridView4.ReadOnly = false;
+            dataGridView4.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+            foreach (DataGridViewColumn column in dataGridView4.Columns)
+            {
+                column.Visible = false;
+            }
+
+            dataGridView4.Columns["Name"].Visible = true;
+            dataGridView4.Columns["Surname"].Visible = true;
+            dataGridView4.Columns["Age"].Visible = true;
+            dataGridView4.Columns["Email"].Visible = true;
+            dataGridView4.Columns["Phone"].Visible = true;
+
+            dataGridView4.Visible = true;
+            dataGridView3.Visible = false;
+            dataGridView1.Visible = false;
+            dataGridView2.Visible = false;
+            button3.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = true;
+            button8.Visible = false;
+            button9.Visible = false;
+            button10.Visible = true;
+
+            CloseConnection();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            dataGridView4.Visible = false;
+            dataGridView3.Visible = false;
+            dataGridView2.Visible = false;
+            dataGridView1.Visible = false;
+            button3.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            button10.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SaveChanges();
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            SaveChanges2();
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            SaveChanges3();
+        }
+        private void SaveChanges()
+        {
+            OpenConnection();
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM Bike", connection);
+            OleDbCommandBuilder builder = new OleDbCommandBuilder(adapter);
+            adapter.Update((DataTable)dataGridView2.DataSource);
+
+            CloseConnection();
+        }
+        private void SaveChanges2()
+        {
+            OpenConnection();
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM Trip", connection);
+            OleDbCommandBuilder builder = new OleDbCommandBuilder(adapter);
+            adapter.Update((DataTable)dataGridView3.DataSource);
+
+            CloseConnection();
+        }
+        private void SaveChanges3()
+        {
+            OpenConnection();
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM Acc", connection);
+            OleDbCommandBuilder builder = new OleDbCommandBuilder(adapter);
+            adapter.Update((DataTable)dataGridView4.DataSource);
+
+            CloseConnection();
+        }
+    }
+}
